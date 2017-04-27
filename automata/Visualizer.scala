02014e4b3264e66ab437b79875f6c80c6d0a89b8
jinyeom <jinyeom95@gmail.com>
2017-04-26 22:01:30 -0500

    added a few stuff

--- automata/Visualizer.scala
+++ automata/Visualizer.scala
@@ -1,8 +1,11 @@
 package automata
 
 import java.awt.geom._
-import javax.swing._;
+import javax.swing._
 
+import scala.collection.mutable.HashMap
+
+// Visualizer given two types for input string and states, and a LogInfo.
 class Visualizer[A, B](log: LogInfo) extends JFrame {
   
   // log type check and cast
@@ -11,20 +14,32 @@ class Visualizer[A, B](log: LogInfo) extends JFrame {
   //
   def logCast(log: LogInfo) = log match {
     case _: FSMLogInfo[A, B] => log.asInstanceOf[FSMLogInfo[A, B]]
-    // case _: NDFSMLogInfo[A, B] => log.asInstanceOf[NDFSMLogInfo[A, B]]
+    // case _: NonDeterministicFSMLogInfo[A, B] => log.asInstanceOf[NonDeterministicFSMLogInfo[A, B]]
   }
+  
+  var _log = logCast(log)             // _log is a casted version of LogInfo.
+  var currState = _log.states().head  // currState points to the current state.
   
   // initialize window
   setTitle("Visual Automata")
   setSize(300, 200)
   setLocationRelativeTo(null)
   
+  // update
   def update() {
     
-    // TODO: implement
+    // TODO: Essentially, move the currState to the next state
     
   }
   
+  // draw
+  def draw() {
+    
+    // TODO: Draw the current states
+    
+  }
+  
+  // start the animation.
   def start() {
     setVisible(true)
   }
